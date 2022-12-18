3db795e33492e04cb97d93786cd43b7c76c38285
MytsV <mytsvictoria@gmail.com>
2022-12-18 17:55:50 +0200

    finished instrument choice

--- lib/main.dart
+++ lib/main.dart
@@ -13,38 +13,102 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Лабораторна №2',
-      home: MainPage(),
+      home: DrawingPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
+class DrawingPage extends StatefulWidget {
+  const DrawingPage({Key? key}) : super(key: key);
 
   @override
-  State<MainPage> createState() => _MainPageState();
+  State<DrawingPage> createState() => _DrawingPageState();
 }
 
-class _MainPageState extends State<MainPage> {
-  Editor _currentEditor = EllipseEditor();
+class _DrawingPageState extends State<DrawingPage> {
+  Editor _currentEditor = PointEditor();
+
+  void _onMenuItemSelected(Editor value) {
+    setState(() {
+      _currentEditor = value;
+    });
+  }
+
+  _getButtonChild(String text, Type editorType) {
+    if (_currentEditor.runtimeType == editorType) {
+      return Row(
+        children: [
+          Text(text),
+          const SizedBox(width: 5,),
+          const Icon(Icons.check, color: Colors.blue),
+        ],
+      );
+    }
+    return Text(text);
+  }
+
+  Widget _getMenuButton() => PopupMenuButton(
+    child: IntrinsicWidth(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text('Об\'єкти'),
+          SizedBox(width: 5,),
+          Icon(Icons.arrow_drop_down, color: Colors.white),
+        ],
+      ),
+    ),
+    itemBuilder: (context) {
+      return [
+        PopupMenuItem<Editor>(
+          value: PointEditor(),
+          child: _getButtonChild('Крапка', PointEditor),
+        ),
+        PopupMenuItem<Editor>(
+          value: LineEditor(),
+          child: _getButtonChild('Лінія', LineEditor),
+        ),
+        PopupMenuItem<Editor>(
+          value: RectangleEditor(),
+          child: _getButtonChild('Прямокутник', RectangleEditor),
+        ),
+        PopupMenuItem<Editor>(
+          value: EllipseEditor(),
+          child: _getButtonChild('Еліпс', EllipseEditor),
+        ),
+      ];
+    },
+    onSelected: _onMenuItemSelected,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Лабораторна №2'),
+        title: _getMenuButton(),
       ),
-      body: GestureDetector(
-        onPanDown: _currentEditor.onPanDown,
-        onPanUpdate: _currentEditor.onPanUpdate,
-        onPanEnd: _currentEditor.onPanEnd,
-        child: SizedBox(
-          width: 300,
-          height: 300,
-          child: ValueListenableBuilder<List<Shape>>(
-            valueListenable: shapes,
-            builder: (context, shapes, _) => CustomPaint(
-              painter: ShapePainter(),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(style: BorderStyle.solid, color: Colors.blue, width: baseStrokeWidth + 1)
+          ),
+          child: GestureDetector(
+            onPanDown: _currentEditor.onPanDown,
+            onPanUpdate: _currentEditor.onPanUpdate,
+            onPanEnd: _currentEditor.onPanEnd,
+            child: ClipRRect(
+              child: SizedBox(
+                width: double.maxFinite,
+                height: double.maxFinite,
+                child: ValueListenableBuilder<List<Shape>>(
+                  valueListenable: shapes,
+                  builder: (context, shapes, _) => CustomPaint(
+                    painter: ShapePainter(),
+                  ),
+                ),
+              ),
             ),
           ),
         ),
@@ -71,6 +135,7 @@ const baseColor = Colors.deepOrange;
 const shadowColor = Colors.black;
 const strokeColor = Colors.black;
 const ellipseFill = Colors.white;
+
 const double baseStrokeWidth = 1;
 
 abstract class Shape {
