2aa42fde84ba6903585c918c485c2d4b7c08e05b
MytsV <mytsvictoria@gmail.com>
2022-12-19 17:09:15 +0200

    implemented table view update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:oop_lab_2/shape_table.dart';
 
 import 'drawing_page.dart';
 
@@ -13,12 +15,141 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Лабораторна №3',
-      home: const DrawingPage(),
+      home: const MainView(),
       //Задаємо основний колір додатку
       theme:
-          ThemeData(primaryColor: Colors.indigo, primarySwatch: Colors.indigo),
+      ThemeData(primaryColor: Colors.indigo, primarySwatch: Colors.indigo),
       //Вимикаємо відображення баннеру "Debug"
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MainView extends StatefulWidget {
+  const MainView({Key? key}) : super(key: key);
+
+  @override
+  _MainViewState createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  bool _tableOpen = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Expanded(child: DrawingPage()),
+          SizedBox(
+            height: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _tableOpen = !_tableOpen;
+                            });
+                          },
+                          icon: Icon(
+                            _tableOpen
+                                ? Icons.close_fullscreen
+                                : Icons.table_chart_outlined,
+                            color: Colors.white,
+                          )),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.upload_file,
+                            color: Colors.white,
+                          )),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.delete_forever,
+                            color: Colors.white,
+                          ))
+                    ],
+                  ),
+                  height: 50,
+                  color: Theme
+                      .of(context)
+                      .primaryColor,
+                ),
+                if (_tableOpen) Expanded(child: TableView()),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TableView extends StatelessWidget {
+  TableView({Key? key}) : super(key: key);
+  final ScrollController _controller = ScrollController();
+
+  List<Widget> _getTableRowChildren(List<String> values, {bool main = false}) {
+    return values
+        .map((e) =>
+        Padding(
+          //Додаємо відступи від тексту
+          padding: const EdgeInsets.all(5),
+          child: Text(
+            e,
+            style: main
+                ? const TextStyle(
+                color: Colors.red, fontWeight: FontWeight.bold)
+                : null,
+          ),
+        ))
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ShapeTable table = ShapeTable.getInstance();
+    return StreamBuilder(
+      stream: table.stream,
+      builder: (context, _) {
+        WidgetsBinding.instance!.addPostFrameCallback((_) {
+          _controller.animateTo(_controller.position.maxScrollExtent, curve: Curves.linear, duration: const Duration(milliseconds: 300));
+        });
+        return SingleChildScrollView(
+          controller: _controller,
+          child: Table(
+            children: [
+              TableRow(
+                  children: _getTableRowChildren(
+                      ['Назва', 'x1', 'y1', 'x2', 'y2'],
+                      main: true)),
+              ...table.entries
+                  .map((e) =>
+                  TableRow(
+                      children: _getTableRowChildren([
+                        e.name,
+                        e.x1.toString(),
+                        e.y1.toString(),
+                        e.x2.toString(),
+                        e.y2.toString()
+                      ])))
+                  .toList()
+            ],
+            //Перший стовпчик буде ширшим у 4 рази за інші
+            columnWidths: const {
+              0: FlexColumnWidth(4),
+            },
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            border: TableBorder.all(color: Colors.black),
+          ),
+        );
+      },
     );
   }
 }
--- lib/shape_table.dart
+++ lib/shape_table.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 class ShapeTable {
   ShapeTable._();
   static final ShapeTable _instance = ShapeTable._();
@@ -6,9 +8,14 @@ class ShapeTable {
   final List<TableEntry> _entries = [];
   List<TableEntry> get entries => List.from(_entries);
 
+  final StreamController<void> _streamController = StreamController<void>.broadcast();
+
   void add(String name, {required int x1, required int x2, required int y1, required int y2}) {
     _entries.add(TableEntry._(name: name, x1: x1, x2: x2, y1: y1, y2: y2));
+    _streamController.add(null);
   }
+
+  get stream => _streamController.stream;
 }
 
 class TableEntry {
